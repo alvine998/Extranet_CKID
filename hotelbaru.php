b711eba2f08a9e78c0791b7526ade08535f2f8bf
alvine <alvinecom2018@gmail.com>
2020-12-16 13:32:50 +0700

    revisi

---
+++ hotelbaru.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Extranet | Carikamar</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="pragma" content="no-cache" />
+    <meta http-equiv="cache-control" content="no-cache" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta http-equiv="content-language" content="en" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+    <script>
+
+    </script>
+
+    <style>
+        a {
+            color: aliceblue;
+            font-size: large;
+        }
+
+        h5,
+        p {
+            color: aliceblue;
+        }
+    </style>
+</head>
+
+<body>
+    <!-- Ini Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light justify-content-center" style="background-color: #229BD7; height: 100px;">
+        <img src="images/Putih_Full_Horizontal.png" width="190px" height="50px">
+    </nav>
+    <br />
+    <!-- Ini Form Registrasi Hotel -->
+    <div class="container">
+        <div class="content">
+            <h1 style="text-align: center;">Daftarkan Hotel Anda Disini</h1>
+            <br />
+            <form>
+                <div class="form-group row">
+                    <label for="staticEmail" class="col-sm-2 col-form-label">Nama Hotel:</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="staticEmail" placeholder="Hotel ...">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="staticEmail" class="col-sm-2 col-form-label">Alamat:</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control" id="staticEmail" placeholder="Jl.Brahmana ...">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="inputState" class="col-sm-2 col-form-label">Provinsi:</label>
+                    <div class="col-sm-2">
+                        <select id="inputState" class="form-control">
+                            <option selected>Pilihan</option>
+                            <option>DKI Jakarta</option>
+                        </select>
+                    </div>
+
+                    <label for="inputState" class="col-sm-1 col-form-label">Kota:</label>
+                    <div class="col-sm-2">
+                        <select id="inputState" class="form-control">
+                            <option selected>Pilihan</option>
+                            <option>DKI Jakarta</option>
+                        </select>
+                    </div>
+
+                    <label for="inputState" class="col-sm-1 col-form-label">Kec:</label>
+                    <div class="col-sm-2">
+                        <select id="inputState" class="form-control">
+                            <option selected>Pilihan</option>
+                            <option>DKI Jakarta</option>
+                        </select>
+                    </div>
+
+                </div>
+                <div class="form-group row">
+                    <label for="inputState" class="col-sm-2 col-form-label">Kel:</label>
+                    <div class="col-sm-2">
+                        <select id="inputState" class="form-control">
+                            <option selected>Pilihan</option>
+                            <option>DKI Jakarta</option>
+                        </select>
+                    </div>
+
+                    <label for="inputState" class="col-sm-1 col-form-label">Kode Pos</label>
+                    <div class="col-sm-2">
+                        <input type="text" class="form-control" id="staticEmail" placeholder="  ">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label for="staticEmail" class="col-sm-2 col-form-label">Fasilitas:</label>
+                    <div class="col-sm-2">
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                            <label class="form-check-label" for="defaultCheck1">
+                                Wifi
+                            </label>
+                        </div>
+
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                            <label class="form-check-label" for="defaultCheck1">
+                                Parkir Mobil
+                            </label>
+                        </div>
+
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                            <label class="form-check-label" for="defaultCheck1">
+                                Parkir Motor
+                            </label>
+                        </div>
+
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                            <label class="form-check-label" for="defaultCheck1">
+                                Kantin / Foodcourt
+                            </label>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-2">
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                            <label class="form-check-label" for="defaultCheck1">
+                                Kolam Renang
+                            </label>
+                        </div>
+
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                            <label class="form-check-label" for="defaultCheck1">
+                                
+                            </label>
+                        </div>
+
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                            <label class="form-check-label" for="defaultCheck1">
+                                Default checkbox
+                            </label>
+                        </div>
+
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="defaultCheck1">
+                            <label class="form-check-label" for="defaultCheck1">
+                                Default checkbox
+                            </label>
+                        </div>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
